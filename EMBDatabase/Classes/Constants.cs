d24a3b781b04f1b69f852db4ba409aa9adac5c27
hinczas <hinczas@gmail.com>
2018-08-13 14:41:23 +0100

    images for parts upload

---
+++ EMBDatabase/Classes/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+
+namespace EMBDatabase.Classes
+{
+    public struct Constants
+    {
+        public const string FILE_TYPE_IMAGE = "Images";
+        public const string FILE_TYPE_DOC = "Documents";
+
+
+        public const string FILE_SENDER_PART = "Parts";
+        public const string FILE_SENDER_MAN = "Manufacturers";
+    }
+}
--- EMBDatabase/Controllers/HomeController.cs
+++ EMBDatabase/Controllers/HomeController.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using EMBDatabase.Context;
+using System;
 using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EMBDatabase.Classes;
 
 namespace EMBDatabase.Controllers
 {
@@ -30,16 +31,33 @@ namespace EMBDatabase.Controllers
                 if (file.ContentLength > 0)
                 {
                     string _FileName = Path.GetFileName(file.FileName);
-                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    string _path = Path.Combine(Server.MapPath("~/Content/Files/"+file_type), _FileName);
+                    string relativePath = "~/Content/Files/" + file_type + "/" + _FileName;
                     file.SaveAs(_path);
+                    if (sender.Equals(Constants.FILE_SENDER_PART))
+                    {
+                        var db = new EMBContext();
+                        var part = db.Part.Find(id);
+                        if (part!=null)
+                        {
+                            part.File.Add(new Models.File()
+                            {
+                                Name = _FileName,
+                                File_Path = relativePath,
+                                File_Type = file_type,
+                                CreateDate = DateTime.Now
+                            });
+                            db.SaveChanges();
+                        }
+                    }
                 }
                 ViewBag.Message = "File Uploaded Successfully!!";
-                return View();
+                return RedirectToAction("Details", sender, new { id = id });
             }
             catch
             {
                 ViewBag.Message = "File upload failed!!";
-                return View();
+                return RedirectToAction("Details", sender, new { id = id });
             }
         }
     }
